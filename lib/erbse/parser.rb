cf779b1cb09cf9bfffa94d55805581c1787dc9d7
Adam Tistler <atistler@datapipe.com>
2017-06-29 10:35:34 -0400

    parser newline fixes

--- lib/erbse/parser.rb
+++ lib/erbse/parser.rb
@@ -1,7 +1,7 @@
 module Erbse
   class Parser
     # ERB_EXPR = /<%(=|\#)?(.*?)%>(\n)*/m # this is the desired pattern.
-    ERB_EXPR = /<%(=+|-|\#|@\s|%)?(.*?)[-=]?%>(\n)*/m # this is for backward-compatibility.
+    ERB_EXPR = /<%(=+|-|\#|@\s|%)?(.*?)[-=]?%>(\n*)/m # this is for backward-compatibility.
     # BLOCK_EXPR     = /\s*((\s+|\))do|\{)(\s*\|[^|]*\|)?\s*\Z/
     BLOCK_EXPR = /\b(if|unless)\b|\sdo\s*$|\sdo\s+\|/
 
@@ -22,12 +22,11 @@ module Erbse
 
       str.scan(ERB_EXPR) do |indicator, code, newlines|
         match = Regexp.last_match
-        len  = match.begin(0) - pos
+        len = match.begin(0) - pos
 
         text = str[pos, len]
-        pos  = match.end(0)
-        ch   = indicator ? indicator[0] : nil
-
+        pos = match.end(0)
+        ch = indicator ? indicator[0] : nil
         if text and !text.strip.empty? # text
           buffers.last << [:static, text]
         end
@@ -40,8 +39,8 @@ module Erbse
             buffers.last << [:dynamic, code]
           end
         elsif ch =~ /#/ # DISCUSS: doesn't catch <% # this %>
-          newlines = code.count("\n")
-          buffers.last.concat  [[:newline]] * newlines if newlines > 0
+          _newlines = code.count("\n")
+          buffers.last.concat [[:newline]] * _newlines if _newlines > 0
         elsif code =~ /\bend\b/ # <% end %>
           buffers.pop
         elsif ch == ?@
@@ -58,7 +57,12 @@ module Erbse
 
         # FIXME: only adds one newline.
         # TODO: does that influence speed?
-        buffers.last << [:newline] if newlines
+        if !newlines.empty? && newlines.count("\n") > 0
+          (1..newlines.count("\n")).each do
+            buffers.last << [:static, "\n"]
+            buffers.last << [:newline]
+          end
+        end
       end
 
       # add text after last/none ERB tag.
